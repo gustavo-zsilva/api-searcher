df4f938980797ddc8a9e22589a67b4eee109da77
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-08-12 18:21:28 -0300

    Applicattion Front-end Finished
    App Front-End finished w/ pure CSS and HTML / responsive layout

---
+++ script.js
@@ -0,0 +1,8 @@
+const input = document.querySelector('.search input')
+const searchButton = document.querySelector('.search-button')
+const selectApp = document.querySelector('.select-app')
+const selectImg = document.querySelector('.select-img')
+
+searchButton.addEventListener('click', () => {
+    
+})
